3146aed6b42a5e3354dc2eabd0accc8d0345c68f
jaesungp <jaesungp@cs.unc.edu>
2016-04-29 15:46:38 -0400

    launch file updated for using Moveit only

--- move_fetch/scripts/prepare_simulated_robot.py
+++ move_fetch/scripts/prepare_simulated_robot.py
@@ -46,6 +46,9 @@ arm_joint_positions  = [1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
 head_joint_names = ["head_pan_joint", "head_tilt_joint"]
 head_joint_positions = [0.0, 0.0]
 
+torso_joint_names = ["torso_lift_joint"]
+torso_joint_positions = [0.38]
+
 if __name__ == "__main__":
     rospy.init_node("prepare_simulated_robot")
 
@@ -67,6 +70,11 @@ if __name__ == "__main__":
     rospy.loginfo("Waiting for gripper_controller...")
     gripper_client = actionlib.SimpleActionClient("gripper_controller/gripper_action", GripperCommandAction)
     gripper_client.wait_for_server()
+    rospy.loginfo("...connected.")
+
+    rospy.loginfo("Waiting for torso_controller...")
+    torso_client = actionlib.SimpleActionClient("torso_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+    torso_client.wait_for_server()
     rospy.loginfo("...connected.")
 
     trajectory = JointTrajectory()
@@ -93,6 +101,18 @@ if __name__ == "__main__":
     arm_goal.trajectory = trajectory
     arm_goal.goal_time_tolerance = rospy.Duration(0.0)
 
+    trajectory = JointTrajectory()
+    trajectory.joint_names = torso_joint_names
+    trajectory.points.append(JointTrajectoryPoint())
+    trajectory.points[0].positions = torso_joint_positions
+    trajectory.points[0].velocities =  [0.0] * len(torso_joint_positions)
+    trajectory.points[0].accelerations = [0.0] * len(torso_joint_positions)
+    trajectory.points[0].time_from_start = rospy.Duration(5.0)
+
+    torso_goal = FollowJointTrajectoryGoal()
+    torso_goal.trajectory = trajectory
+    torso_goal.goal_time_tolerance = rospy.Duration(0.0)
+
     gripper_goal = GripperCommandGoal()
     gripper_goal.command.max_effort = 10.0
     gripper_goal.command.position = 0.1
@@ -101,6 +121,8 @@ if __name__ == "__main__":
     head_client.send_goal(head_goal)
     arm_client.send_goal(arm_goal)
     gripper_client.send_goal(gripper_goal)
+    torso_client.send_goal(torso_goal)
+    torso_client.wait_for_result(rospy.Duration(5.0))
     gripper_client.wait_for_result(rospy.Duration(5.0))
     arm_client.wait_for_result(rospy.Duration(6.0))
     head_client.wait_for_result(rospy.Duration(6.0))
